b0415d0b1284517cecacbd3e772080eaa179f2b0
Marc Meseguer <marc.meseguer@becloud.local>
2018-10-10 17:51:38 +0200

    Test-SWConnection first release

---
+++ PSSonicWall/Private/Test-SWConnection.ps1
@@ -0,0 +1,20 @@
+# Tests if current SonicWall connection is valid
+Function Test-SWConnection {
+    # If an existing connection exists we test it
+    if ($env:SWConnection) {
+        Try {
+            # Make a dummy request
+            Invoke-RestMethod -Uri "$($env:SWConnection)config/pending" -Method Get -ContentType 'application/json' | Out-Null
+            # If we don't get an error we return $true
+            Return $true
+        }
+        Catch {
+            # If we cannot connect to the previously established connection throw an error
+            Throw "Your previous connection is disconnected. Use Connect-SWAppliance to connect again."
+        }
+    }
+    # If there's no connection string throw an error
+    else {
+        Throw "You are not connected to any SonicWall Appliance. Use Connect-SWAppliance to connect."
+    }
+}
