dfb7fd180b87df9d5c9cd475d5170113e5b3b4f8
Marc Meseguer <marc.meseguer@becloud.local>
2018-10-11 15:24:44 +0200

    Deleted trailing whitespaces.

--- PSSonicWall/Private/Convert-AddressObject.ps1
+++ PSSonicWall/Private/Convert-AddressObject.ps1
@@ -13,15 +13,15 @@ function ConvertFrom-AddressObject {
         ForEach ($Object in $Objects) {
             # Getting the nested properties in $Type
             $NestedProperties = $Object.$Type.PSObject.Properties.Name
-   
+
             # Flatting property $Type
             ForEach ($NestedProperty in $NestedProperties) {
                 $Object | Add-Member -Name $NestedProperty -Type NoteProperty -Value $Object.$Type.$NestedProperty
             }
 
-            # Remove nested property 
+            # Remove nested property
             $Object.PSObject.Properties.Remove($type)
         }
         Return $Objects
-    } 
+    }
 }
