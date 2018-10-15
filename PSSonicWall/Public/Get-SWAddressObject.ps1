617559b434486a15709a59e4b372d9127e63510b
Marc Meseguer <marc.meseguer@becloud.local>
2018-10-15 20:41:39 +0200

    Added support to pipeline address groups in Get-SWAddressObject.

--- PSSonicWall/Public/Get-SWAddressObject.ps1
+++ PSSonicWall/Public/Get-SWAddressObject.ps1
@@ -2,15 +2,18 @@ function Get-SWAddressObject {
     [CmdletBinding()]
     param (
         # Type of address object
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$true,ParameterSetName='byType')]
         [ValidateSet('host','range','network','mac','fqdn')]
         [string]$Type,
         # Version type for the query
         [ValidateSet('ipv4','ipv6')]
         [string]$IpVersion ='ipv4',
-        # Name of the address object
-        [Parameter(ValueFromPipelineByPropertyName)]
-        [string]$Name
+        # Name of the object
+        [Parameter(Mandatory=$true,ParameterSetName='byName')]
+        [string]$Name,
+        # Use pipelined object
+        [Parameter(Mandatory=$true,ValueFromPipeline,ParameterSetName='byInputObject')]
+        [PSCustomObject]$RelatedObject
     )
     begin {
         # Testing if a connection to SonicWall exists
@@ -32,28 +35,76 @@ function Get-SWAddressObject {
         $IpSubTypes = 'host','range','network'
     }
     process {
-        # If it's an IP subtype we add the IP version to the resource
-        if ($IpSubtypes -contains $Type) {
-            $Resource = "$BaseResource/$IpVersion"
-            # Querying for address objects
-            $Result = (Invoke-RestMethod -Uri "$SWBaseUrl$Resource" -Method $Method -ContentType $ContentType).address_objects.$IpVersion | Where-Object {$_.PSobject.Properties.Name -contains $Type}
-            if ($Result) {
-                $Result = ConvertFrom-AddressObject -Object $Result -Type $Type
+        switch ($PSCmdlet.ParameterSetName){
+            'byType' {
+                # If it's an IP subtype we add the IP version to the resource
+                if ($IpSubtypes -contains $Type) {
+                    $Resource = "$BaseResource/$IpVersion"
+                    # Querying for address objects
+                    $Result = (Invoke-RestMethod -Uri "$SWBaseUrl$Resource" -Method $Method -ContentType $ContentType).address_objects.$IpVersion | Where-Object {$_.PSobject.Properties.Name -contains $Type}
+                    if ($Result) {
+                        $Result = ConvertFrom-AddressObject -Object $Result -Type $Type
+                    }
+                }
+                # If not just build the resource with the type
+                else {
+                    $Resource = "$BaseResource/$Type"
+                    # Querying for address objects
+                    $Result = (Invoke-RestMethod -Uri "$SWBaseUrl$Resource" -Method $Method -ContentType $ContentType).address_objects.$Type
+                }
+                # Filter by name if introduced
+                if ($Name) {
+                    $Result = $Result | Where-Object Name -eq $Name
+                }
+                # Return the results
+                $Result
+            }
+            'byName' {
+                # Declaration of the Address Object types
+                $ObjectTypes = @('ipv4','ipv6','mac','fqdn')
+                # Loop through the types
+                foreach ($ObjectType in $ObjectTypes) {
+                    # Build of the resource
+                    $Resource = "$BaseResource/$ObjectType/name/$Name"
+                    # Try to make the request. If it works we exit the loop, if it fails it means that it doesn't exist in this $ObjectType, so we continue.
+                    Try {
+                        $Result = (Invoke-RestMethod -Uri "$SWBaseUrl$Resource" -Method $Method -ContentType $ContentType).address_object.$ObjectType
+                        Break
+                    }
+                    Catch {
+                        Continue
+                    }
+                }
+                # If the loop ended without a result we Throw an error
+                if (!$Result) {
+                    Throw "Object '$Name' not found."
+                }
+                # Return the results
+                $Result
+            }
+            'byInputObject' {
+                # Getting the name of the object
+                $Names = $RelatedObject | Select-Object -ExpandProperty 'object_Name'
+                # Loop through Names names
+                foreach ($Name in $Names) {
+                    # Declaration of the Address Object types
+                    $ObjectTypes = @('ipv4','ipv6','mac','fqdn')
+                    # Loop through the types
+                    foreach ($ObjectType in $ObjectTypes) {
+                        # Build of the resource
+                        $Resource = "$BaseResource/$ObjectType/name/$Name"
+                        # Try to make the request. If it works we exit the loop, if it fails it means that it doesn't exist in this $ObjectType, so we continue.
+                        Try {
+                            $Result = (Invoke-RestMethod -Uri "$SWBaseUrl$Resource" -Method $Method -ContentType $ContentType).address_object.$ObjectType
+                            $Result
+                            Break
+                        }
+                        Catch {
+                            Continue
+                        }
+                    }
+                }
             }
         }
-        # If not just build the resource with the type
-        else {
-            $Resource = "$BaseResource/$Type"
-            # Querying for address objects
-            $Result = (Invoke-RestMethod -Uri "$SWBaseUrl$Resource" -Method $Method -ContentType $ContentType).address_objects.$Type
-        }
-
-        # Filter by name if introduced
-        if ($Name) {
-            $Result = $Result | Where-Object Name -eq $Name
-        }
-
-        # Return the results
-        $Result
     }
 }
