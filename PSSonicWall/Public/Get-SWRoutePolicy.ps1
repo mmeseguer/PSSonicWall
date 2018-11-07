50e80c1cdf17b050d0e7cd01d2cab1342cf92935
Marc Meseguer <marc.meseguer@becloud.local>
2018-11-07 14:45:18 +0100

    Get-SWRoutePolicy first release.

---
+++ PSSonicWall/Public/Get-SWRoutePolicy.ps1
@@ -0,0 +1,43 @@
+function Get-SWRoutePolicy {
+    [CmdletBinding()]
+    param (
+        # Version type for the query
+        [ValidateSet('ipv4','ipv6','all')]
+        [string]$IpVersion ='ipv4'
+    )
+    begin {
+        # Testing if a connection to SonicWall exists
+        Test-SWConnection
+
+        # Declaring used rest method
+        $Method = 'get'
+
+        # Declaring the base resource
+        $BaseResource = 'route-policies'
+
+        # Declaring the content type
+        $ContentType = 'application/json'
+
+        # Declaring IP Types
+        $IpVersions = 'ipv4','ipv6'
+
+        # Getting the base URL of our connection
+        $SWBaseUrl = $env:SWConnection
+    }
+    process {
+        # If we are not querying a certain ip type show it
+        if ($IpVersion -ne 'all') {
+            $Resource = "$BaseResource/$IpVersion"
+            $Result = (Invoke-RestMethod -Uri "$SWBaseUrl$Resource" -Method $Method -ContentType $ContentType).route_policies.$IpVersion
+        }
+        # If we are querying all the types loop through them
+        else {
+            ForEach ($IpVersion in $IpVersions) {
+                $Resource = "$BaseResource/$IpVersion"
+                $Result += (Invoke-RestMethod -Uri "$SWBaseUrl$Resource" -Method $Method -ContentType $ContentType).route_policies.$IpVersion
+            }
+        }
+        # Return the result
+        $Result
+    }
+}
