a4846beea6e626bd6c60dc1588acad7383cf2270
Marc Meseguer <mmeseguer87@gmail.com>
2019-02-11 15:59:43 +0100

    Remove-SWPendingChange first release.

---
+++ PSSonicWall/Public/Remove-SWPendingChange.ps1
@@ -0,0 +1,42 @@
+function Remove-SWPendingChange {
+    <#
+    .SYNOPSIS
+    Removes pending changes from SonicWall Appliance.
+
+    .DESCRIPTION
+    This function removes the changes in the staging area from a SonicWall appliance.
+
+    .EXAMPLE
+    Remove-SWPendingChange
+    Remove all the changes in the staging area pending of commit.
+
+    #>
+    [CmdletBinding()]
+    param (
+    )
+
+    begin {
+        # Testing if a connection to SonicWall exists
+        Test-SWConnection
+
+        # Declaring used rest method
+        $Method = 'delete'
+
+        # Declaring the base resource
+        $BaseResource = 'config'
+
+        # Declaring the content type
+        $ContentType = 'application/json'
+
+        # Getting the base URL of our connection
+        $SWBaseUrl = $env:SWConnection
+    }
+    process {
+        # Query for pending changes
+        $Resource = "$BaseResource/pending"
+        $Result = Invoke-RestMethod -Uri "$SWBaseUrl$Resource" -Method $Method -ContentType $ContentType
+
+        # Return the result
+        $Result
+    }
+}
