83675cdd9dba925d5144ec2c93e663e91b4fd5a9
Marc Meseguer <mmeseguer87@gmail.com>
2019-02-11 16:26:29 +0100

    Set-SWPendingChange first commit.

---
+++ PSSonicWall/Public/Set-SWPendingChange.ps1
@@ -0,0 +1,44 @@
+function Set-SWPendingChange {
+    <#
+    .SYNOPSIS
+    Apply's pending changes from SonicWall Appliance.
+
+    .DESCRIPTION
+    This function apply's the changes in the staging area from a SonicWall appliance.
+
+    .EXAMPLE
+    Set-SWPendingChange
+    Apply all the changes in the staging area pending of commit.
+
+    #>
+    [CmdletBinding()]
+    param (
+    )
+
+    begin {
+        # Testing if a connection to SonicWall exists
+        Test-SWConnection
+
+        # Declaring used rest method
+        $Method = 'post'
+
+        # Declaring the base resource
+        $BaseResource = 'config'
+
+        # Declaring the content type
+        $ContentType = 'application/json'
+
+        # Getting the base URL of our connection
+        $SWBaseUrl = $env:SWConnection
+
+        # Building full resource
+        $Resource = "$BaseResource/pending"
+    }
+    process {
+        # Query for pending changes
+        $Result = Invoke-RestMethod -Uri "$SWBaseUrl$Resource" -Method $Method -ContentType $ContentType
+
+        # Return the result
+        $Result
+    }
+}
